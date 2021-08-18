fd37b9be21ae470f6b74e551c1d88cb1d55d2c9e
tusheet <tgoli@sametunes.com>
2021-08-18 13:04:40 -0400

    updated resume.js and added new images

--- Tusheet Website/src/Components/resume.js
+++ Tusheet Website/src/Components/resume.js
@@ -1,16 +1,16 @@
 let resume = {
     "imageUrl" : "",
     "name" : "Tusheet Goli",
-    "role" : "Senior at Georgia Tech, BSCS",
+    "role" : "Senior at Georgia Tech, BSCS/MSCS",
     "profile_pic" : "public_images/TusheetImage.jpg",
     "linkedIn_ID" : "https://www.linkedin.com/in/tusheet",
     "email" : "tgoli3@gatech.edu",
     "tel_number" : "+1 470-350-4410",
-    "aboutMe" : "Enthusiastic student of computer science with a passion to explore the application of the sciences, the power of technology & unleash immense opportunities." +
-        " I am passionate about application of Artificial Intelligence & Information Networks in industry. " +
-        "My experience at agile growth oriented environments has provided me the opportunity to put my coding knowledge to enhance user experiences and" +
-        " contribute towards client critical projects. " +
-        "I am eager to pursue opportunities that stretch my interests in full stack development and in Natural Language Processing research. ",
+    "aboutMe" : "Enthusiastic student of computer science with a passion to explore the application of the sciences and the power of technology. " +
+        "I am passionate about application of Artificial Intelligence, Machine Learning & Information Networks in industry. " +
+        "My experience at agile growth oriented environments has provided me the opportunity to put my coding knowledge to enhance user experiences and " +
+        "contribute towards client critical projects. " +
+        "I am eager to pursue opportunities that stretch my interests in full stack development and Machine Learning applications in industry.",
     "socials" : [{
         "name" : "linkedin",
         "url" : "https://www.linkedin.com/in/tusheet",
@@ -20,7 +20,6 @@ let resume = {
         "name" : "github",
         "url" : "http://www.github.com/Tusheet10Goli",
         "className" : "fab fa-github"
-
     },
     {
         "name" : "email",
@@ -28,13 +27,13 @@ let resume = {
         "className" : "fas fa-envelope"
     }],
     "address" : "Atlanta, GA",
-    "website" : "",
+    "website" : "http://tusheet-goli.github.io/portfolio",
     "education" : [
         {
             "University" : "Georgia Institute of Technology",
-            "Degree" : "Bachelors of Science",
+            "Degree" : "Bachelors of Science and Masters of Science",
             "Major" : "Computer Science",
-            "Threads" : "Intelligence and Information Info-Networks",
+            "Threads" : "Intelligence, Machine Learning, and Information Info-Networks",
             "GradMonth" : "December",
             "GradYear" : "2022",
             "GPA" : "4.0",
@@ -86,64 +85,119 @@ let resume = {
     ],
     "work" : [
         {
-            "employer" : "SameTunes LLC",
-            "position" : "Head of API Design | Co-Founder",
+            "employer" : "YugabyteDB",
+            "position" : "Platform and Cloud Intern",
             "description" : "Co-founding member of SameTunes LLC, a startup from Georgia Tech’s Create-X program. An online\n" +
                 "portal for music compatibility driven through robust algorithm based interests match, SameTunes gained\n" +
-                "over 9k users at launch. \n Analysed user stats using Spotify's API. \nFull Stack Development on critical components of the website. \n" +
-                "Coordinated a team of four developers to create back end schema and related database structures.\nFront-end: Bootstrap & CSS, Backend: server-side scripting using php, and database management: MYSQL",
-            "url" : "http://www.sametunes.com",
-            "image" : "public_images/Sametunes.PNG"
+                "over 15k users at launch.\n Analyzed user stats using Spotify's API.\nFull Stack Development on critical components of the website.\n" +
+                "Coordinated a team of four developers to create back-end schema and related database structures.\nFront-end: Bootstrap & CSS, Backend: server-side scripting using php, and database management: MYSQL",
+            "url" : "https://www.yugabyte.com/",
+            "image" : "public_images/yugabyte_logo.PNG"
         },
         {
-            "employer" : "Shipa Freight",
-            "position" : "Data Science Intern",
-            "description" : "Interned at a start-up digital freight forwarder, focused on online freight solutions in the emerging markets.\n" +
-                "Provided insights to the sales team on customer behavior through data analysis on customer data.\n" +
-                "Feature engineering using pandas, matplotlib, & numpy on large data sets to create meaningful trends.\n" +
-                "Predictive analysis using machine learning along with clustering to help product positioning and marketing",
-            "url" : "http://www.shipafreight.com",
-            "image" : "public_images/Shipa_Freight.png"
+            "employer" : "SameTunes LLC",
+            "position" : "Co-Founder | Head of Algorithm Design",
+            "description" : "Co-founding member of SameTunes LLC, a startup from Georgia Tech’s Create-X program. An online\n" +
+                "portal for music compatibility driven through robust algorithm based interests match, SameTunes gained\n" +
+                "over 15k users at launch.\nAnalyzed user stats using Spotify's API.\nFull Stack Development on critical components of the website.\n" +
+                "Coordinated a team of four developers to create back-end schema and related database structures.\nFront-end: Bootstrap & CSS, Back-end: " +
+                "server-side scripting using php, and database management: MYSQL",
+            "url" : "http://www.sametunes.com",
+            "image" : "public_images/sametunes_logo.PNG"
+        },
+        {
+            "employer" : "Hisense USA",
+            "position" : "Software Engineer Intern",
+            "description" : "Worked in the Research and Development Center with Hisense USA to create system and consumer applications.\n" +
+                "Worked on ACR (Automatic Content Recognition) for Hisense TVs which is a Picture and Audio Quality AI technology that recognizes TV content\n" +
+                "and launches appropriate customized settings for video and audio to enhance the overall user experience.\n" + 
+                "Developed an Android TV usage recording application that records the usage statistics, usage patterns, and tendencies of the user to suggest the\n" +
+                "provide user interested content.\n" + "Redesigned the Hisense Product Registration to support one-click device registration.",
+            "url" : "https://www.hisense-usa.com/",
+            "image" : "public_images/hisense_logo.PNG"
         },
         {
             "employer" : "Automated Algorithm Design",
             "position" : "Undergraduate Research Assistant",
-            "description" : "Automated Algorithm Design | Natural language processing using genetic algorithms.| Project sponsored\n" +
-                "by Google.\n" +
-                "Implemented genetic learning on Automated Algorithm Design using machine learning.\n" +
-                "Created algorithms for natural language processing to analyze sentiment data from IMDB reviews.\n" +
-                "Analyzed NHS Chest X-Ray data to accurately predict the onset of diseases.",
+            "description" : "Automated Algorithm Design is a Google sponsored research that NPL and CV application in Genetic Machine Learning Algorithms." +
+                "Implement NLP applications to analyze meaning and sentiment in text data using Genetic Machine Learning.\n" +
+                "Analyzed NHS Chest X-Ray data to accurately predict the onset of diseases and increased evaluation accuracy\n" +
+                "by 27% by creating mating and mutation functions for CIFAR-10 X-Ray CV dataset.",
             "url" : "https://www.vip.gatech.edu/teams/automated-algorithm-design",
-            "image" : "public_images/AAD_VIP.PNG"
+            "image" : "public_images/ml_image.PNG"
         }
     ],
     "timelineData" : [
         {
-            "title" : "Shipa Freight",
-            "text" : "Concepts: Python, Feature Engineering, Data Manipulation, Numpy, Scikit-learn",
-            "date" : "Summer 2019",
+            "title" : "TA: Advanced Algorithm Design",
+            "text" : "Concepts: Java, Advanced Algorithms, Dynamic Programming, Project Organization and Creating, Student Management",
+            "date" : "Aug 2021 - Present",
+            "category" : {
+                "tag": "Teaching Assistant",
+                "color": "#ef8110"
+            },
+            "link" : {url:"https://gt-cs-3510.github.io/syllabus.html", text:"CS 3510 - Advanced Algorithm Design"},
+            "image_url" : "public_images/java.png"
+        },
+        {
+            "title" : "YugabyteDB",
+            "text" : "Concepts: Java, Python, Linux, Ansible, JS, SQL, Scala, Feature Design and Management",
+            "date" : "May 2021 - Aug 2021",
             "category" : {
                 "tag": "Internship",
                 "color": "#018f69"
             },
-            "link" : {url:"https://www.shipafreight.com/", text : "Shipa Freight"},
-            "image_url" : "public_images/Shipa_Freight.png"
+            "link" : {url:"https://www.yugabyte.com/", text : "YugabyteDB"},
+            "image_url" : "public_images/yugabyte_logo.PNG"
+        },
+        {
+            "title" : "TA: Data Structures and Algorithms",
+            "text" : "Concepts: Java, Data Structures, Algorithms, Project Organization and Creating, Student Management",
+            "date" : "Aug 2020 - May 2021",
+            "category" : {
+                "tag": "Teaching Assistant",
+                "color": "#ef8110"
+            },
+            "link" : {url:"https://catalog.gatech.edu/courses-undergrad/cs/", text:"CS 1332 - Data Structures and Algorithms"},
+            "image_url" : "public_images/java.png"
+        },
+        {
+            "title" : "SameTunes LLC",
+            "text" : "Concepts: PHP, Bootstrap, React, SQL, Algorithm Design, API Design, Agile Development, Web Development",
+            "date" : "May 2020 - Aug 2020",
+            "category" : {
+                "tag": "Internship",
+                "color": "#018f69"
+            },
+            "link" : {url:"https://www.sametunes.com", text:"SameTunes: Social Music"},
+            "image_url" : "public_images/sametunes_logo.PNG"
+        },
+        {
+            "title" : "Hisense USA",
+            "text" : "Concepts: Android Development, Java, System Services, Research & Development",
+            "date" : "May 2019 - Aug 2019",
+            "category" : {
+                "tag": "Internship",
+                "color": "#018f69"
+            },
+            "link" : {url:"https://www.hisense-usa.com/", text:"Hisense USA"},
+            "image_url" : "public_images/hisense_logo.PNG"
         },
         {
             "title" : "Automated Algorithm Design Research Project",
-            "text" : "Concepts: Genetic Algorithms, NLP, Research Principles, Machine Learning",
+            "text" : "Concepts: Machine Learning, Genetic Algorithms, NLP, CV, Research Principles",
             "date" : "Aug 2019 - Dec 2020",
             "category" : {
                 "tag": "Research",
                 "color": "#1ecbe1"
             },
-            "link" : {url : "https://www.vip.gatech.edu/teams/automated-algorithm-design", text : "Automated Algorithm Design Research Team"},
+            "link" : {url : "https://www.vip.gatech.edu/teams/automated-algorithm-design", text : "Automated Algorithm Design Research"},
             "image_url" : "public_images/AAD_VIP.PNG"
         },
         {
             "title" : "TA: Object Oriented Programming",
             "text" : "Concepts: Java, Object Oriented Programming, Project Organization and Creating, Student Management",
-            "date" : "Jan 2020 - Present",
+            "date" : "Aug 2019 - Aug 2020",
             "category" : {
                 "tag": "Teaching Assistant",
                 "color": "#ef8110"
@@ -151,17 +205,6 @@ let resume = {
             "link" : {url:"https://www.cc.gatech.edu/~stasko/1331/", text:"CS 1331 - Object Oriented Programming"},
             "image_url" : "public_images/java.png"
         },
-        {
-            "title" : "SameTunes LLC",
-            "text" : "Concepts: PHP, Twitter Bootstrap, SQL, SCSS, API Design, Agile Development, Website Design",
-            "date" : "May 2020 - Aug 2020",
-            "category" : {
-                "tag": "Internship",
-                "color": "#018f69"
-            },
-            "link" : {url:"https://www.sametunes.com", text:"SameTunes: Social Music"},
-            "image_url" : "public_images/Sametunes.PNG"
-        }
     ]
 }
 export default resume;
